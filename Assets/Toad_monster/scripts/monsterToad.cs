11da4e70682450284e7ed7756805099f0c17c1b5
Fernando <fernando.cardenasm@gmail.com>
2016-01-07 01:15:08 +0100

    Add monster and monster's movement

---
+++ Assets/Toad_monster/scripts/monsterToad.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class monsterToad : MonoBehaviour {
+
+    //Code for movement taken from http://answers.unity3d.com/questions/14279/make-an-object-move-from-point-a-to-point-b-then-b.html
+
+    public Transform farEnd;
+    private Vector3 frometh;
+    private Vector3 untoeth;
+    private float secondsForOneLength = 5f;
+
+    // Use this for initialization
+    void Start () {
+        frometh = transform.position;
+        untoeth = farEnd.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(frometh, untoeth,
+        Mathf.SmoothStep(0f, 1f,
+        Mathf.PingPong(Time.time / secondsForOneLength, 1f)
+        ));
+    }
+}
